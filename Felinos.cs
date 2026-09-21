650ce988baab5046134857a00343e47fbfe81f42
Ceci1012 <rodriguesdesouzacecilia2@gmail.com>
2026-09-21 14:18:13 -0300

    bluh bluh

--- Felinos.cs
+++ Felinos.cs
@@ -6,7 +6,7 @@ namespace SistemaFelinos
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Raça { get; set; }
+        public string Raça { get; set; }                          
         public string Pelagem { get; set; }
         public int Idade { get; set; }
 
